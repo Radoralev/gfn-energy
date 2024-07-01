6d0a3c048a25f3e51fd1461d2bb9c323a23bae93
radoslavralev <radoslavralev@tum.de>
2024-07-01 09:53:05 +0200

    added fed z learned

--- energy_sampling/analyze_results.py
+++ energy_sampling/analyze_results.py
@@ -1,15 +1,11 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, r2_score
 import numpy as np
 import argparse
-import pandas as pd
-import seaborn as sns
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, r2_score
-import numpy as np
+
+import matplotlib.pyplot as plt
 
 # Function to parse values with uncertainties
 def parse_value_with_uncertainty(value):
@@ -18,8 +14,8 @@ def parse_value_with_uncertainty(value):
 
 # Create argument parser
 parser = argparse.ArgumentParser(description='Analyze results')
-parser.add_argument('input', type=str, help='Path to the input CSV file')
-parser.add_argument('output', type=str, help='Path to save the output plot')
+parser.add_argument('--input', type=str, help='Path to the input CSV file')
+parser.add_argument('--output', type=str, help='Path to save the output plot')
 
 # Parse command line arguments
 args = parser.parse_args()
@@ -31,6 +27,7 @@ data = pd.read_csv(args.input)
 data[['experimental_val_mean', 'experimental_val_uncertainty']] = data['experimental_val'].apply(lambda x: pd.Series(parse_value_with_uncertainty(x)))
 data[['fed_Z_mean', 'fed_Z_uncertainty']] = data['fed_Z'].apply(lambda x: pd.Series(parse_value_with_uncertainty(x)))
 data[['fed_Z_lb_mean', 'fed_Z_lb_uncertainty']] = data['fed_Z_lb'].apply(lambda x: pd.Series(parse_value_with_uncertainty(x)))
+data[['fed_Z_learned_mean', 'fed_Z_learned_uncertainty']] = data['fed_Z_learned'].apply(lambda x: pd.Series(parse_value_with_uncertainty(x)))
 
 # Exclude rows with extremely large fedZ or fedZlb values
 data = data[(data['fed_Z_mean'].abs() < 100) & (data['fed_Z_lb_mean'].abs() < 100)]
@@ -40,6 +37,7 @@ experimental_val = data['experimental_val_mean']
 experimental_uncertainty = data['experimental_val_uncertainty']
 fed_Z = data['fed_Z_mean']
 fed_Z_lb = data['fed_Z_lb_mean']
+fed_Z_learned = data['fed_Z_learned_mean']
 
 # Function to plot scatter plot with linear regression and statistics
 def plot_scatter(ax, x, y, xerr, title):
@@ -61,11 +59,12 @@ def plot_scatter(ax, x, y, xerr, title):
   ax.set_ylabel('ΔG_calc, kcal/mol')
 
 # Create subplots
-fig, axes = plt.subplots(1, 2, figsize=(18, 6))
+fig, axes = plt.subplots(1, 3, figsize=(18, 6))
 
 # Plot each method
 plot_scatter(axes[0], experimental_val, fed_Z, experimental_uncertainty, 'fed_Z')
 plot_scatter(axes[1], experimental_val, fed_Z_lb, experimental_uncertainty, 'fed_Z_lb')
+plot_scatter(axes[2], experimental_val, fed_Z_learned, experimental_uncertainty, 'fed_Z_learned')
 
 # Adjust layout and show plot
 plt.tight_layout()
