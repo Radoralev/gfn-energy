7a975080eb0305bc8f78a18f3206a2e5b305375e
Radoslav Ralev <ge49kan@mytum.de>
2024-09-19 12:46:05 +0200

    mcmc eval utils

--- energy_sampling/mcmc_eval_utils.py
+++ energy_sampling/mcmc_eval_utils.py
@@ -13,9 +13,8 @@ kB = unit.BOLTZMANN_CONSTANT_kB.value_in_unit(unit.hartree/unit.kelvin)
 hartree_to_kcal = 627.509  # Conversion factor from Hartree to kcal/mol
 beta = 1/(kB * T)  # Inverse temperature
 
-v_energy = MoleculeFromSMILES_XTB(' CCCCCC(=O)OC', temp=T, solvate=False)
-s_energy = MoleculeFromSMILES_XTB(' CCCCCC(=O)OC', temp=T, solvate=True)
-
+v_energy = MoleculeFromSMILES_XTB('CCCCCC(=O)OC', temp=T, solvate=False)
+s_energy = MoleculeFromSMILES_XTB('CCCCCC(=O)OC', temp=T, solvate=True)
 def calc_fed(energies, beta_applied=False):
     en = energies if beta_applied else energies*beta
     factor = hartree_to_kcal * kB * T 
@@ -23,7 +22,7 @@ def calc_fed(energies, beta_applied=False):
     return free_energy
 
 
-def brute_force_fed(smiles, T, ground_truth, uncertainty, num_batches=100, batch_size=32):
+def brute_force_fed(smiles, T, ground_truth, uncertainty, num_batches=100, batch_size=32, beta_applied=False):
     v_energy = MoleculeFromSMILES_XTB(smiles, temp=T, solvate=False)
     s_energy = MoleculeFromSMILES_XTB(smiles, temp=T, solvate=True)
 
@@ -35,29 +34,29 @@ def brute_force_fed(smiles, T, ground_truth, uncertainty, num_batches=100, batch
     distr = torch.distributions.VonMises(torch.tensor([0.0]*n), torch.tensor([4.0]*n))
     # create a tensor to store the energies
     samples = distr.sample((num_batches*batch_size,))
-    v_energies = torch.zeros(num_batches*batch_size)
-    s_energies = torch.zeros(num_batches*batch_size)
-    start = 0
-    end = batch_size
-    for i in tqdm(range(num_batches)):
-        batch = samples[start:end]
-        v_energies[start:end] = v_energy.energy(batch) 
-        s_energies[start:end] = s_energy.energy(batch)
+    # v_energies = torch.zeros(num_batches*batch_size)
+    # s_energies = torch.zeros(num_batches*batch_size)
+    # start = 0
+    # end = batch_size
+    # for i in tqdm(range(num_batches)):
+    #     batch = samples[start:end]
+    #     v_energies[start:end] = v_energy.energy(batch) 
+    #     s_energies[start:end] = s_energy.energy(batch)
 
-        if (i+1) % 25 == 0:
-            print('Batch {}/{}'.format(i+1, num_batches))
-            v_free_energy = calc_fed(v_energies[:end])
-            s_free_energy = calc_fed(s_energies[:end])
-            fed = s_free_energy - v_free_energy
-            print('Error for {}: {}'.format(smiles, fed - ground_truth))
+    #     if (i+1) % 25 == 0:
+    #         print('Batch {}/{}'.format(i+1, num_batches))
+    #         v_free_energy = calc_fed(v_energies[:end])
+    #         s_free_energy = calc_fed(s_energies[:end])
+    #         fed = s_free_energy - v_free_energy
+    #         print('Error for {}: {}'.format(smiles, fed - ground_truth))
 
-        start = end
-        end += batch_size
+    #     start = end
+    #     end += batch_size
 
-    v_free_energy = calc_fed(v_energies)
-    s_free_energy = calc_fed(s_energies)
-    fed = s_free_energy - v_free_energy
-    return fed, samples
+    # v_free_energy = calc_fed(v_energies, beta_applied=beta_applied)
+    # s_free_energy = calc_fed(s_energies, beta_applied=beta_applied)
+    # fed = s_free_energy - v_free_energy
+    return None, samples
 
 # https://en.wikipedia.org/wiki/Bennett_acceptance_ratio#The_basic_case
 def bar_estimate(v_file, s_file, beta, subsample=0):
@@ -70,8 +69,8 @@ def bar_estimate(v_file, s_file, beta, subsample=0):
         s_states = s_states[indices]
 
 
-    v_energy = MoleculeFromSMILES_XTB(' CCCCCC(=O)OC', temp=T, solvate=False)
-    s_energy = MoleculeFromSMILES_XTB(' CCCCCC(=O)OC', temp=T, solvate=True)
+    v_energy = MoleculeFromSMILES_XTB('CCCCCC(=O)OC', temp=T, solvate=False)
+    s_energy = MoleculeFromSMILES_XTB('CCCCCC(=O)OC', temp=T, solvate=True)
     vv_energies = torch.zeros(len(v_states))
     vs_energies = torch.zeros(len(v_states))
     sv_energies = torch.zeros(len(v_states))
@@ -141,38 +140,7 @@ def likelihood_s(angle_0, angle_1, angle_2, angle_3, angle_4, angle_5, angle_6, 
 
     # Compute log-probabilities as -energy (since energy is proportional to -log(prob))
     log_prob = -(energy_s).item()  # Return log-probability
-    return log_prob
-
-
-def run_mcmc(likelihood, n_dims, tries=100, min_max=(-np.pi, np.pi), Rminus1_stop=0.05):
-
-    # Cobaya config for sampling the likelihood based on torsion angles
-    config = {
-        "likelihood": {"gfn": likelihood},  # Likelihood function using GFN
-        "sampler": {"mcmc": {"max_tries": tries}},  # Sampler using MCMC
-        "params": {
-            f"angle_{i}": {
-                "prior": {"min": min_max[0], "max": min_max[1]},  # Range of angles
-                "ref": 0,  # Starting point for each angle
-                "proposal": 0.02  # Step size for angle proposals
-            }
-            for i in range(n_dims)  # Loop over the number of dimensions (torsion angles)
-        },
-        "sampler": {
-            "mcmc": {
-                "Rminus1_stop": Rminus1_stop, "max_tries": 1000
-            }
-        }
-    }
-
-    # Run the Cobaya MCMC sampler with the defined config
-    updated_info, sampler = run(config)
-
-    # Results are stored in `updated_info` and `sampler`
-    print("Updated information: ", updated_info)
-
-    samples = sampler.products()['sample'][['angle_0', 'angle_1', 'angle_2', 'angle_3', 'angle_4', 'angle_5', 'angle_6', 'angle_7']].values
-    return samples, sampler
+    return log_prob        
 
 def fed_estimate_Z(energy_v, energy_s, beta_applied=False):
     kT_logZ_vacuum = calc_fed(energy_v, beta_applied=beta_applied)
@@ -188,3 +156,110 @@ def fed_estimate_Z_from_file(energy_v_file, energy_s_file, beta_applied=False):
     energy_s = torch.from_numpy(np.load(energy_s_file))
 
     return fed_estimate_Z(energy_v, energy_s, beta_applied=beta_applied)
+
+def weighted_EXP(energies_A, energies_B, weights):
+    exponents = (energies_A - energies_B) # sv is solvation energies on vacuum samples 
+    weighted = torch.exp(exponents) * weights
+    weighted_sum = weighted.sum()
+    return torch.log(weighted_sum)
+
+def compute_weights(energies, pfs, beta=1):
+    exponents = energies - pfs.sum(dim=-1)
+    exponents -= exponents.max()
+    weights = torch.exp(exponents)
+    weights /= weights.sum()
+    return weights
+
+def calc_ESS(energies, pfs, beta):
+    weights = compute_weights(energies, pfs, beta)
+    NSS = torch.exp(-torch.sum(weights*torch.log(weights)))
+    N = len(energies)
+    return NSS,NSS/N
+
+def compute_free_energy_difference(ss_energies, sv_energies, vs_energies, vv_energies, tol=1e-6, max_iter=100):
+    """
+    Estimates the free energy difference between solvation and vacuum using the BAR method.
+
+    Args:
+        ss_energies: List or array of beta*energy for solvation energies computed on solvation states.
+        sv_energies: List or array of beta*energy for solvation energies computed on vacuum states.
+        vs_energies: List or array of beta*energy for vacuum energies computed on solvation states.
+        vv_energies: List or array of beta*energy for vacuum energies computed on vacuum states.
+        tol: Tolerance for convergence in the bisection method.
+        max_iter: Maximum number of iterations in the bisection method.
+
+    Returns:
+        df: The estimated free energy difference between solvation and vacuum.
+    """
+
+    # Convert inputs to numpy arrays
+    ss_energies = np.array(ss_energies)
+    sv_energies = np.array(sv_energies)
+    vs_energies = np.array(vs_energies)
+    vv_energies = np.array(vv_energies)
+
+    # Calculate energy differences
+    dV_p = ss_energies - sv_energies  # Delta V for solvation ensemble
+    dV_0 = vs_energies - vv_energies  # Delta V for vacuum ensemble
+
+    # Since energies are already beta * energy, we set beta = 1
+    beta = 1.0
+
+    # Initial guesses for df using exponential averages
+    exponent_p = -beta * dV_p
+    exponent_0 = -beta * dV_0
+
+    max_exp_p = np.max(exponent_p)
+    max_exp_0 = np.max(exponent_0)
+
+    # Shift exponents for numerical stability
+    exp_p = np.exp(exponent_p - max_exp_p)
+    exp_0 = np.exp(exponent_0 - max_exp_0)
+
+    df_p = - (np.log(np.sum(exp_p)) + max_exp_p - np.log(len(dV_p))) / beta
+    df_0 = - (np.log(np.sum(exp_0)) + max_exp_0 - np.log(len(dV_0))) / beta
+
+    # Initialize interval for bisection
+    df_min = min(df_p, df_0)
+    df_max = max(df_p, df_0)
+
+    def bar_residual(df):
+        exponent_p = beta * (dV_p - df)
+        exponent_0 = beta * (-dV_0 + df)
+        gf = 1.0 / (1.0 + np.exp(exponent_p))
+        gr = 1.0 / (1.0 + np.exp(exponent_0))
+        return np.mean(gf) - np.mean(gr)
+
+    res_min = bar_residual(df_min)
+    res_max = bar_residual(df_max)
+
+    # Expand interval if necessary
+    if res_min * res_max > 0:
+        df_expand = 1.0
+        while res_min * res_max > 0 and df_expand < 1e6:
+            df_min -= df_expand
+            df_max += df_expand
+            res_min = bar_residual(df_min)
+            res_max = bar_residual(df_max)
+            df_expand *= 2
+        if res_min * res_max > 0:
+            raise ValueError('Cannot find a valid interval for bisection.')
+
+    # Bisection method to solve for df
+    for _ in range(max_iter):
+        df_mid = 0.5 * (df_min + df_max)
+        res_mid = bar_residual(df_mid)
+        if abs(res_mid) < tol:
+            df = df_mid
+            break
+        if res_min * res_mid < 0:
+            df_max = df_mid
+            res_max = res_mid
+        else:
+            df_min = df_mid
+            res_min = res_mid
+    else:
+        df = df_mid
+        print('Warning: Maximum iterations reached in bisection method.')
+
+    return df
